3e6ac3c47b5e80742da055aeaeb277e733e5225b
Cristian Morataya <cris.morataya@gmail.com>
2019-05-14 08:57:11 -0600

    Add functions, arguments and scripting

---
+++ 00_chapterTwo/arguments.py
@@ -0,0 +1,9 @@
+import sys
+
+arguments = sys.argv
+print(arguments)
+print("The number of arguments passed was: %s" % (str(len(arguments))))
+i = 0
+for x in arguments:
+    print("The %d argument is %s" % (i, x))
+    i += 1
